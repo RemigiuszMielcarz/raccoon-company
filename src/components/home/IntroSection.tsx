95a846d081fbca2d273ecd1bd136dc1b9a9349b2
Remik <remigiusz.mielcarz@gmail.com>
2023-09-07 19:19:27 +0200

    Intro section testimonials

--- src/components/home/IntroSection.tsx
+++ src/components/home/IntroSection.tsx
@@ -3,6 +3,8 @@ import Typed from "typed.js";
 import { motion, AnimatePresence } from "framer-motion";
 import ScrollDown from "../ScrollDown";
 import { fadeInOut } from "../../utils/motion";
+import Divider from "../Divider";
+import { ReactComponent as Stars } from "../../images/icons/stars.svg";
 
 const IntroSection: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -48,7 +50,7 @@ const IntroSection: React.FC = () => {
   return (
     <section className="container lg:px-0">
       <div
-        className="container relative lg:px-0 flex items-center bg-cover lg:bg-contain rounded-[8px] lg:rounded-none"
+        className="container relative flex items-center bg-cover lg:bg-contain rounded-[8px] lg:rounded-none"
         style={{
           height: "860px",
           backgroundImage: `url(${process.env.PUBLIC_URL}/images/backgrounds/home.png)`,
@@ -56,8 +58,8 @@ const IntroSection: React.FC = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <div className="lg:pl-[50px]">
-          <p className="text-xxxl text-white">
+        <div className="flex flex-col gap-[60px] lg:gap-[80px] w-full">
+          <p className="text-xxxl 2xl:text-[100px] 2xl:leading-[100px] text-white">
             Stwórzmy razem
             <br />
             <span
@@ -65,6 +67,18 @@ const IntroSection: React.FC = () => {
               ref={typeTarget}
             ></span>
           </p>
+          <Divider variant={"grey"} />
+          <div className="flex gap-4 items-center">
+            <div className="flex items-center gap-[10px] p-[10px] rounded-[20px] bg-white">
+              <div className="w-[100px]">
+                <Stars />
+              </div>
+              <p className="font-bold text-title text-display">5.0</p>
+            </div>
+            <p className="text-md lg:text-lg text-white">
+              Nasi Klienci kochają współpracę z nami
+            </p>
+          </div>
         </div>
         <AnimatePresence mode="wait">
           {isVisible && (
